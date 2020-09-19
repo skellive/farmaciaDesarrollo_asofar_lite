7670d466857a05f3d1f03e7ea8120c388baf8afd
iabotprueba <63275364+iabotprueba@users.noreply.github.com>
2020-09-19 00:01:18 -0500

    commit
    excel

--- src/com/farmacia/dao/conexion_Excel.java
+++ src/com/farmacia/dao/conexion_Excel.java
@@ -10,6 +10,7 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 
 import java.io.IOException;
+import static java.lang.Double.parseDouble;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
@@ -27,45 +28,55 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 public class conexion_Excel {
 
     public static void subirArchivosProductos(String ruta) throws IOException, SQLException, ClassNotFoundException {
-        try{
-        Conexion c = new Conexion();
+        try {
+            Conexion c = new Conexion();
 
-        Connection con = c.conectar();
-        PreparedStatement ps;
-        FileInputStream file = new FileInputStream(new File(ruta));
+            Connection con = c.conectar();
+            PreparedStatement ps;
+            FileInputStream file = new FileInputStream(new File(ruta));
 
-        XSSFWorkbook wb = new XSSFWorkbook(file);
-        XSSFSheet sheet = wb.getSheetAt(0);
+            XSSFWorkbook wb = new XSSFWorkbook(file);
+            XSSFSheet sheet = wb.getSheetAt(0);
 
-        int numFilas = sheet.getLastRowNum();
-        for (int a = 1; a <= numFilas; a++) {
+            int numFilas = sheet.getLastRowNum();
+            for (int a = 1; a <= numFilas; a++) {
                 Row fila = sheet.getRow(a);
-                ps = con.prepareStatement("INSERT INTO productos_app (codigo_barras, nombre, descripcion, peso, id_tipo, id_medidas, id_categoria, id_marcas, estado, id_usuario, iva, cantidad_minima, receta, unidades, registro_actualizado, precio_producto_app, id_presentacion) VALUES(?,?,?,?,?,?,?,?,'A',1001,?,?,?,?,'S',?,?)");//17
-                ps.setDouble(1, fila.getCell(0).getNumericCellValue());
+                ps = con.prepareStatement("INSERT INTO productos_app (codigo_barras, nombre, descripcion, peso, id_tipo, id_medidas, id_categoria, id_marcas, estado, id_usuario, iva, cantidad_minima, receta, unidades, registro_actualizado, precio_producto_app, id_presentacion) VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)");//17
+                ps.setString(1, fila.getCell(0).getStringCellValue());
                 ps.setString(2, fila.getCell(1).getStringCellValue());
                 ps.setString(3, fila.getCell(2).getStringCellValue());
                 ps.setDouble(4, fila.getCell(3).getNumericCellValue());
-                ps.setString(5, fila.getCell(4).getStringCellValue());
-                ps.setString(6, fila.getCell(5).getStringCellValue());
-                ps.setString(7, fila.getCell(6).getStringCellValue());
-                ps.setString(8, fila.getCell(7).getStringCellValue());                
-                
+
+                ps.setString(9, "A");
+                ps.setString(10, "1001");
                 ps.setString(11, fila.getCell(8).getStringCellValue());
-                ps.setDouble(12, fila.getCell(9).getNumericCellValue());
+                
                 ps.setString(13, fila.getCell(10).getStringCellValue());
-                ps.setDouble(14, fila.getCell(11).getNumericCellValue());                
-                ps.setDouble(16, fila.getCell(12).getNumericCellValue());
-                ps.setDouble(17, fila.getCell(13).getNumericCellValue());
-                ps.execute();             
-        
-        }
+                
+                ps.setString(15, "S");
+                try {
+                    ps.setLong(5, Long.getLong("" + fila.getCell(4).getNumericCellValue()));
+                    ps.setLong(6, Long.getLong("" + fila.getCell(5).getNumericCellValue()));
+                    ps.setLong(7, Long.getLong("" + fila.getCell(6).getNumericCellValue()));                    
+                    ps.setLong(8, Long.getLong("" + fila.getCell(7).getNumericCellValue()));
+                    ps.setLong(12, Long.getLong("" +fila.getCell(9).getNumericCellValue()));
+                    ps.setLong(14, Long.getLong("" +fila.getCell(11).getNumericCellValue()));
+                    ps.setLong(16, Long.getLong("" + fila.getCell(12).getNumericCellValue()));
+                    ps.setString(17, fila.getCell(13).getStringCellValue());
+                } catch (SQLException e) {
+                    JOptionPane.showMessageDialog(null, e);
+                }
+
+                ps.execute();
+
+            }
             JOptionPane.showMessageDialog(null, "Datos guardados correctamente");
-        
-        }catch(FileNotFoundException ex){
+
+        } catch (FileNotFoundException ex) {
             Logger.getLogger(conexion_Excel.class.getName()).log(Level.SEVERE, null, ex);
             JOptionPane.showMessageDialog(null, "Error al procesar:" + ex);
         }
-        
+
     }
 
 }
