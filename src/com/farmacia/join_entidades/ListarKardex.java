b8deab44dfde1fbde8f27d9f3805aa20501e334b
galojose38 <galojose38@gmail.com>
2020-10-01 10:23:35 -0500

    Para Listar Kardex

--- src/com/farmacia/join_entidades/ListarKardex.java
+++ src/com/farmacia/join_entidades/ListarKardex.java
@@ -2,5 +2,135 @@ package com.farmacia.join_entidades;
 
 
 public class ListarKardex {
+    
+    //12 variables
+    Long id_kardex;
+    Long id_producto;
+    String nombre_Producto;
+    String descripcion;
+    String tipo;
+    Long cantidad;
+    String iva;
+    Double precio_compra;
+    Double precio_venta;
+    Double total;
+    Long compra;
+    Long venta;
+
+    public ListarKardex() {
+    }
+
+    public ListarKardex(Long id_kardex, Long id_producto, String nombre_Producto, String descripcion, String tipo, Long cantidad, String iva, Double precio_compra, Double precio_venta, Double total, Long compra, Long venta) {
+        this.id_kardex = id_kardex;
+        this.id_producto = id_producto;
+        this.nombre_Producto = nombre_Producto;
+        this.descripcion = descripcion;
+        this.tipo = tipo;
+        this.cantidad = cantidad;
+        this.iva = iva;
+        this.precio_compra = precio_compra;
+        this.precio_venta = precio_venta;
+        this.total = total;
+        this.compra = compra;
+        this.venta = venta;
+    }
+
+    public Long getId_kardex() {
+        return id_kardex;
+    }
+
+    public void setId_kardex(Long id_kardex) {
+        this.id_kardex = id_kardex;
+    }
+
+    public Long getId_producto() {
+        return id_producto;
+    }
+
+    public void setId_producto(Long id_producto) {
+        this.id_producto = id_producto;
+    }
+
+    public String getNombre_Producto() {
+        return nombre_Producto;
+    }
+
+    public void setNombre_Producto(String nombre_Producto) {
+        this.nombre_Producto = nombre_Producto;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public Long getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(Long cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public String getIva() {
+        return iva;
+    }
+
+    public void setIva(String iva) {
+        this.iva = iva;
+    }
+
+    public Double getPrecio_compra() {
+        return precio_compra;
+    }
+
+    public void setPrecio_compra(Double precio_compra) {
+        this.precio_compra = precio_compra;
+    }
+
+    public Double getPrecio_venta() {
+        return precio_venta;
+    }
+
+    public void setPrecio_venta(Double precio_venta) {
+        this.precio_venta = precio_venta;
+    }
+
+    public Double getTotal() {
+        return total;
+    }
+
+    public void setTotal(Double total) {
+        this.total = total;
+    }
+
+    public Long getCompra() {
+        return compra;
+    }
+
+    public void setCompra(Long compra) {
+        this.compra = compra;
+    }
+
+    public Long getVenta() {
+        return venta;
+    }
+
+    public void setVenta(Long venta) {
+        this.venta = venta;
+    }
+    
+    
 
 }
