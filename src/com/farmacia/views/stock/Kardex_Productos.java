c2987b4bd63b18025834ebc1a2716b342e375f1b
galojose38 <galojose38@gmail.com>
2020-10-08 16:39:45 -0500

    Kardex

--- src/com/farmacia/views/stock/Kardex_Productos.java
+++ src/com/farmacia/views/stock/Kardex_Productos.java
@@ -198,7 +198,7 @@ public class Kardex_Productos extends javax.swing.JDialog {
         jLabel1.setFont(new java.awt.Font("Tahoma", 1, 24)); // NOI18N
         jLabel1.setForeground(new java.awt.Color(255, 255, 255));
         jLabel1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        jLabel1.setText("PRODUCTOS EN STOCK");
+        jLabel1.setText("PRODUCTOS EN INVENTARIO");
         jLabel1.setOpaque(true);
         jLabel1.addMouseMotionListener(new java.awt.event.MouseMotionAdapter() {
             public void mouseDragged(java.awt.event.MouseEvent evt) {
